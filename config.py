2918f5ff3dd0d7e21c2f78098139fd01982c3c54
Gtend <86248339+Gtend@users.noreply.github.com>
2025-08-14 15:05:36 +0900

    build: config.py

--- config.py
+++ config.py
@@ -3,11 +3,16 @@ import os
 
 load_dotenv()
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DB_DIR = os.path.join(BASE_DIR, 'database')
+
+# database 폴더 생성 (없으면 만들기)
+os.makedirs(DB_DIR, exist_ok=True)
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'database', 'interview.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DB_DIR, 'interview.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     KAKAO_CLIENT_ID = os.getenv('KAKAO_CLIENT_ID')
     KAKAO_CLIENT_SECRET = os.getenv('KAKAO_CLIENT_SECRET')
     KAKAO_REDIRECT_URI = os.getenv('KAKAO_REDIRECT_URI')
-    print(KAKAO_REDIRECT_URI)
