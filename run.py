ed51a0978ee20e92235768fc176da5fbcc730b82
jeonghyeonmin <75874847+jeonghyeonmin1@users.noreply.github.com>
2025-08-15 23:32:12 +0900

    revise: app.run

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    # app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
+    app.run(debug=False, host='0.0.0.0',use_reloader=False, port=8080)
